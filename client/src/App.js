45757725c7f86244ab6847b888972225935a31e4
akshay <akshay.nalawade@developersinstitute.co.nz>
2022-12-13 22:21:50 +1300

    react app and express

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const h1 = <h1>Hello React</h1>
+  return (
+    <div className="App">
+      <header className="App-header">
+        {h1}
+      </header>
+    </div>
+  );
+}
+
+export default App;
