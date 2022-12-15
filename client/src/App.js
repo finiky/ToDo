e5d4aef9493d58eef9b047434734660962509ab6
akshay <akshay.nalawade@developersinstitute.co.nz>
2022-12-15 14:25:57 +1300

    set up api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const h1 = <h1>Hello React</h1>
+  const h1 = <h1>Hello React</h1>;
+  const h2 = React.createElement("h2", {}, "Hello Again");
   return (
     <div className="App">
       <header className="App-header">
         {h1}
+        {h2}
       </header>
     </div>
   );
