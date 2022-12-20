44413d40091b1dd21b712c7f96e2577794c5810b
akshay <akn2612@gmail.com>
2022-12-21 02:04:33 +1300

    created signup component

---
+++ client/src/components/Signup.js
@@ -0,0 +1,9 @@
+import {useState } from "react";
+const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+};
+
+export default Signup;
--- sql/BEGIN TRANSACTION;.sql
+++ sql/BEGIN TRANSACTION;.sql
@@ -1,16 +1,16 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE users {
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username text,
     email text,
     passkey text
-};
+);
 
-CREATE TABLE todos {
+CREATE TABLE todos (
     id SERIAL PRIMARY KEY,
     task text,
     todos_user_id INT REFERENCES users(id)
-};
+);
 
 COMMIT;
